6c490108973d412fd90336a9f351b67588a52d60
Moin Malik <Moin Malik>
2023-02-14 12:45:21 +0530

    14 feb

--- src/main/java/com/example/polls/PollsApplication.java
+++ src/main/java/com/example/polls/PollsApplication.java
@@ -8,6 +8,7 @@ public class PollsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PollsApplication.class, args);
+		System.out.println("Hello world... ");
 	}
 
 }
