811815ad8967b4536e887b30d7839ea1584e2582
Moin Malik <Moin Malik>
2023-02-15 21:13:23 +0530

    15 feb

--- src/main/java/com/example/polls/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/polls/security/JwtAuthenticationFilter.java
@@ -1,4 +1,72 @@
 package com.example.polls.security;
 
+import io.jsonwebtoken.io.IOException;
+import org.hibernate.annotations.common.util.impl.LoggerFactory;
+import org.jboss.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StreamUtils;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.net.http.HttpResponse;
+
 public class JwtAuthenticationFilter {
+
+
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+
+    private static final Logger logger = LoggerFactory.logger(JwtAuthenticationFilter.class);
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpResponse response, FilterChain filterChain) throws
+            ServletException, IOException, java.io.IOException {
+        try {
+            String jwt = getJwtFromRequest(request);
+
+            if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
+                Long userId = tokenProvider.getUserIdFromJWT(jwt);
+
+                UserDetails userDetails = customUserDetailsService.loadUserById(userId);
+
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            }
+        }catch (Exception ex) {
+            logger.info("Could not set user authentication in security context", ex);
+        }
+
+        filterChain.doFilter((ServletRequest) request, (ServletResponse) response);
+
+
+    }
+
+    private String getJwtFromRequest(HttpServletRequest request) {
+
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer")) {
+            return bearerToken.substring(7, bearerToken.length());
+        }
+
+        return null;
+    }
 }
