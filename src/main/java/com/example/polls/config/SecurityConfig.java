9d993711b3078cd221362106a81ad563df925a19
Moin Malik <Moin Malik>
2023-02-15 12:22:07 +0530

    15 feb

---
+++ src/main/java/com/example/polls/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.example.polls.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+public class SecurityConfig extends WebSecurityConfiguration {
+
+
+
+}
