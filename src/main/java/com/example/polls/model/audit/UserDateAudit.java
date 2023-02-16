bf201f281b30b6b144cc5792a59b5abffb4e599e
Moin Malik <Moin Malik>
2023-02-16 21:49:29 +0530

    16 feb

--- src/main/java/com/example/polls/model/audit/UserDateAudit.java
+++ src/main/java/com/example/polls/model/audit/UserDateAudit.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.LastModifiedBy;
 
+import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
 
@@ -15,25 +16,26 @@ import javax.persistence.MappedSuperclass;
 )
 public abstract class UserDateAudit extends DateAudit {
 
-//    @CreatedBy
-//    private Long createdBy;
-//
-//    @LastModifiedBy
-//    private Long updatedBy;
-//
-//    public Long getCreatedBy() {
-//        return createdBy;
-//    }
-//
-//    public void setCreatedBy(Long createdBy) {
-//        this.createdBy = createdBy;
-//    }
-//
-//    public Long getUpdatedBy() {
-//        return updatedBy;
-//    }
-//
-//    public void setUpdatedBy(Long updatedBy) {
-//        this.updatedBy = updatedBy;
-//    }
+    @CreatedBy
+    @Column(updatable = false)
+    private Long createdBy;
+
+    @LastModifiedBy
+    private Long updatedBy;
+
+    public Long getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(Long createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Long getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(Long updatedBy) {
+        this.updatedBy = updatedBy;
+    }
 }
